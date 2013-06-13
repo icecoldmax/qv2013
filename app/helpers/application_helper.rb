a84f4108e9f8477adaa551ec44af19860e1eb433
David Nettle <david.nettle@gmail.com>
2013-06-13 16:10:05 +0930

    full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title ona per-page basis.
+  def full_title(page_title)
+    base_title = "QuizVids"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,39 +4,44 @@ describe "Static Pages" do
   
   describe "Home page" do
     
-    it "should have the content 'QuizVids'" do
+    it "should have the H1 'QuizVids'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'QuizVids')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "QuizVids | Home")
+      page.should have_selector('title', :text => "QuizVids")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Setup page" do
-    it "should have the content 'Setup your quiz'" do
+    it "should have the H1 'Setup your quiz'" do
       visit '/static_pages/setup'
       page.should have_selector('h1', :text => 'Setup your quiz')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/setup'
-      page.should have_selector('title', :text => "QuizVids | Setup")
+      page.should have_selector('title', :text => "QuizVids")
     end
   end
 
   describe "Quiz page" do
 
-    it "should have the content 'Quiz Time!'" do
+    it "should have the H1 'Quiz Time!'" do
       visit '/static_pages/quiz'
       page.should have_selector('h1', :text => "Quiz Time!")
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/quiz'
-      page.should have_selector('title', :text => "QuizVids | Quiz")
+      page.should have_selector('title', :text => "QuizVids")
     end
   end
 
