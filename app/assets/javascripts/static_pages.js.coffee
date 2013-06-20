99d55ea632fdfa355e6d4a1c2cdc3ddd29156523
David Nettle <david.nettle@gmail.com>
2013-06-20 15:41:25 +0930

    Youtube API

--- app/assets/javascripts/static_pages.js.coffee
+++ app/assets/javascripts/static_pages.js.coffee
@@ -3,3 +3,38 @@
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 # alert gon.ass
+
+$ ->
+
+  if current_page is 'quiz'
+
+    tag = document.createElement('script');
+    tag.src = "http://www.youtube.com/player_api";
+    firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+    playVideo = ->
+      ytplayer.playVideo() if ytplayer
+
+    pauseVideo = ->
+      ytplayer.pauseVideo() if ytplayer
+
+    stopVideo = ->
+      ytplayer.stopVideo() if ytplayer
+
+    muteVideo = ->
+      ytplayer.mute() if ytplayer
+
+    unmuteVideo = ->
+      ytplayer.unMute() if ytplayer
+
+    loadVideo = (id) ->
+      ytplayer.loadVideoById(id) if ytplayer
+
+
+    window.playVideo = playVideo
+    window.pauseVideo = pauseVideo
+    window.stopVideo = stopVideo
+    window.muteVideo = muteVideo
+    window.unmuteVideo = unmuteVideo
+    window.loadVideo = loadVideo
---
+++ app/assets/javascripts/youtubeapi.js
@@ -0,0 +1,48 @@
+function onYouTubePlayerAPIReady() {
+  console.log("onyoutubeplayerapiready called");
+
+    ytplayer = new YT.Player('ytplayer', {
+      width: '100%',
+      height: '100%',
+      //videoId: getParameterByName('vid0'),
+      playerVars: { 'autoplay': 0, 'controls': 0, 'wmode': 'opaque',
+                  //'playlist': vids.join(),
+                    // 'origin': 'http://localhost:3000/',
+                    'modestbranding': 1, 'showinfo': 0 
+                  },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange,
+        'onError': onPlayerError
+              }
+    });
+  }
+
+  function onPlayerReady(evt) {
+    // setInterval(updatePlayerInfo, 1000);
+    // updatePlayerInfo();
+    // interval = getParameterByName('interval');
+    // ytplayer.loadPlaylist(vids);
+    //initIFrameClick();
+    //  evt.target.playVideo();
+    console.log("Player is ready");
+  }
+   
+  function onPlayerStateChange(evt) {
+    console.log("player state change");
+    // var newState = evt.data;
+    // updateHTML("playerState", newState);
+    // console.log("newState: " + newState);
+    // if (newState == 0 || newState == 2 || newState == 3 || newState == 5) {
+    //   clearInterval(t);
+    //   console.log("clearInterval ran (state change)");
+    // } else if (newState == 1) {
+    //   t = setInterval(stopAtTime, 1000, interval);
+    // }
+
+  }
+
+  // This function is called when an error is thrown by the player
+  function onPlayerError(errorCode) {
+    alert("An error occured of type:" + errorCode);
+  }
